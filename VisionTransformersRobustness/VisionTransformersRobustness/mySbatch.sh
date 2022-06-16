ac976fbdf7506e8cf12ed3e413342e5bff9e3b98
saigontrade88 <longdangvn88@gmail.com>
2022-06-16 05:38:48 -0400

    Update mySbatch.sh

--- VisionTransformersRobustness/VisionTransformersRobustness/mySbatch.sh
+++ VisionTransformersRobustness/VisionTransformersRobustness/mySbatch.sh
@@ -3,14 +3,14 @@
 #SBATCH --output=out_ViT_%j.txt
 #SBATCH --error=err_ViT_%j.txt
 #SBATCH --cpus-per-task=3
-#SBATCH --gres=gpu:2
-#SBATCH --mem=90G
-#SBATCH --time=20:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --mem=120G
+#SBATCH --time=5:00:00
 #SBATCH --mail-user=longdang@usf.edu
 #SBATCH --mail-type=END,FAIL,REQUEUE
-#SBATCH --partition=simmons_itn18 
-#SBATCH --qos=preempt_short
-#SBATCH --array=0-1
+#SBATCH --partition=snsm_itn19  
+#SBATCH --qos=snsm19_special
+#SBATCH --array=0-2
 
 source $HOME/.bashrc
 
@@ -21,7 +21,7 @@ conda activate torch_171
 
 echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
 
-METHODS=(3)
+METHODS=(1 2 3)
 echo "JOBID: $SLURM_JOB_ID"
 echo "METHOD: ${METHODS[$SLURM_ARRAY_TASK_ID]}"
 
