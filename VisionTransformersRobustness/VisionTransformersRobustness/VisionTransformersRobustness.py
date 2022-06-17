34b88bc470621689249bf06c8508fbf364e14b43
saigontrade88 <longdangvn88@gmail.com>
2022-06-16 21:36:28 -0400

    Update VisionTransformersRobustness.py

--- VisionTransformersRobustness/VisionTransformersRobustness/VisionTransformersRobustness.py
+++ VisionTransformersRobustness/VisionTransformersRobustness/VisionTransformersRobustness.py
@@ -1,15 +1,41 @@
 import os
 import sys
 import DefaultMethods
-os.environ["CUDA_VISIBLE_DEVICES"]="0, 1"
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
 #Main method to do one of the CIFAR-10 experiments 
 #Uncomment any one of the following lines to run an attack (RayS, Adaptive or SAGA) 
 #The attack can be run on either ViT-L-16 or a defense made up of ViT-L-16 and BiT-M-R101x3 
 def main(myInt):
+    parser = argparse.ArgumentParser(description = 'ViT and Robustness')
+    parser.add_argument('-methodID', '--methodID', default=0, type=int, help='method ID')
+    parser.add_argument('-xVal', '--xVal', default='data/0_RayS_Attack/valLoader_X.npy' , type=str, help='xVal data')
+    parser.add_argument('-yVal', '--yVal', default='data/0_RayS_Attack/valLoader_Y.npy', type=str, help='yVal data')
+    parser.add_argument('-xClean', '--xClean', default='data/0_RayS_Attack/cleanLoader_X.npy',type=str, help='xClean correctly classified data')
+    parser.add_argument('-yClean', '--yClean', default='data/0_RayS_Attack/cleanLoader_Y.npy',type=str, help='yClean correctly classified data')
+    parser.add_argument('-qLimit', '--queryLimit', default=100, type=int, help='yAdv data')
+    parser.add_argument('-xAdv', '--xAdv', type=str, help='xAdv data')
+    parser.add_argument('-yAdv', '--yAdv', type=str, help='yAdv data')
+    args = parser.parse_args()
+
+
+    # Collect arguments
+    myInt = args.methodID
+    xVal = args.xVal
+    yVal = args.yVal
+
+    xClean = args.xClean
+    yClean = args.yClean
+
+    qLimit = args.qLimit
+
+    xAdv = args.xAdv
+    yAdv = args.yAdv
+
     if myInt == 0:
         #Uncomment next line to do the RayS attack on the Vision Transformer, ViT-L-16
-        DefaultMethods.RaySAttackVisionTransformer()
+        # RaySAttackVisionTransformer(xValData, yValData, xCleanData, yCleanData)
+        DefaultMethods.RaySAttackVisionTransformer(xVal, yVal, xClean, yClean, qLimit)
 
     elif myInt == 1:
         #Uncomment next line to do the RayS attack on the Shuffle Defense (ViT-L-16 and BiT-M-R101x3)
@@ -36,5 +62,5 @@ def main(myInt):
 
 if __name__ == "__main__":
 
-    main(int(sys.argv[1]))
+    main()
     
